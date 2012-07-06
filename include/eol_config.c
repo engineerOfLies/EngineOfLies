f3a9cac0861534c8a332219b0ab4126cb7feb677
Mike Meehan <mjmeehan@gmail.com>
2012-07-06 17:55:25 -0600

    First batch of changes for GHashTables for config

--- include/eol_config.c
+++ include/eol_config.c
@@ -8,7 +8,7 @@ eolBool _eol_config_initialized;
 
 /*local functions*/
 void eol_config_deinit(void);
-void eol_config_parse_tier(yaml_parser_t *parser, GNode *cfg);
+void eol_config_parse_tier(yaml_parser_t *parser, GHashTable *cfg);
 
 /*function definitions*/
 
@@ -32,9 +32,9 @@ eolConfig *eol_config_load(char* filename)
     FILE *input;
     eolConfig *config = malloc(sizeof(eolConfig));
     strcpy(config->filename, filename);
-    config->_node = g_node_new(filename);
+    config->_node = g_hash_table_new(&g_str_hash, &g_str_equal);
     if(config->_node == NULL) {
-        fputs("ERROR: Unable to allocate GNode for config", stderr);
+        fputs("ERROR: Unable to allocate GHash for config", stderr);
         return NULL;
     }
     if(!yaml_parser_initialize(&parser)) {
@@ -68,34 +68,39 @@ void eol_config_destroy(eolConfig *config)
     free(config);
 }
 
-void eol_config_parse_tier(yaml_parser_t *parser, GNode *cfg)
+void eol_config_parse_tier(yaml_parser_t *parser, GHashTable *cfg)
 {
-    GNode *last_leaf = cfg;
+    gchar *last_key;
     yaml_event_t event;
+    GHashTable *mapping;
     /* First element must be a variable, or we'll change states to SEQ */
-    int state = VAR;
+    int state = KEY;
     do {
         yaml_event_delete(&event);
         yaml_parser_parse(parser, &event);
         switch(event.type) {
         case YAML_SCALAR_EVENT:
-            if (state == VAR) {
-                /* new variable, create a new element */
-                last_leaf = g_node_append(cfg, g_node_new(g_strdup((gchar*) event.data.scalar.value)));
+            if (state == KEY) {
+                /* new key, hold on to it until we get a value as well */
+                last_key = (g_strdup((gchar*) event.data.scalar.value));
             } else {
                 /* state is VAL or SEQ */
-                g_node_append_data(last_leaf, g_strdup((gchar*) event.data.scalar.value));
+		/* TODO data type logic should go here */
+                g_hash_table_insert(cfg, last_key, g_strdup((gchar*) event.data.scalar.value));
             }
-            state ^= VAL; /* Toggles VAR/VAL, avoids touching SEQ */
+            state ^= VAL; /* Toggles KEY/VAL, avoids touching SEQ */
             break;
         case YAML_SEQUENCE_START_EVENT:
             state = SEQ;
             break;
         case YAML_SEQUENCE_END_EVENT:
-            state = VAR;
+            state = KEY;
             break;
         case YAML_MAPPING_START_EVENT:
-            eol_config_parse_tier(parser, last_leaf);
+	    /* create a child hash table and recurse */
+	    mapping = g_hash_table_new(&g_str_hash, &g_str_equal);
+	    g_hash_table_insert(cfg, last_key, mapping);
+            eol_config_parse_tier(parser, mapping);
             state ^= VAL;
             break;
         case YAML_MAPPING_END_EVENT:
@@ -116,7 +121,7 @@ void eol_config_parse_tier(yaml_parser_t *parser, GNode *cfg)
 
 }
 
-void eol_config_dump(char* filename, GNode* data)
+void eol_config_dump(char* filename, GHashTable* data)
 {
     /* NOP */
 }
@@ -124,9 +129,9 @@ void eol_config_dump(char* filename, GNode* data)
 
 eolInt eol_config_get_int_by_tag( eolConfig *conf, eolLine tag)
 {
-	GNode *gndata = g_node_find(conf->_node, G_PRE_ORDER, G_TRAVERSE_ALL, tag);
+	GHashTable *gndata = g_hash_table_lookup(conf->_node, tag);
 	if(gndata != NULL) {
-		return (eolInt) gndata->data;
+		return (eolInt) gndata;
 	} else {
 		printf("Config tag %s not found in %s\n", tag, conf->filename);
 		return (eolInt) NULL;
